ce5be569b03186ba3e8b74c4fdc0767050ee29cc
FlavioKde <149841602+FlavioKde@users.noreply.github.com>
2025-07-10 23:34:52 +0200

    Initial commit: configured dependencies and Spring Boot structure

---
+++ src/main/java/cat/itacademy/s05/blackjack/S05t01n01BlackjackApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s05.blackjack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S05t01n01BlackjackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S05t01n01BlackjackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/itacademy/s05/blackjack/S05t01n01BlackjackApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s05.blackjack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S05t01n01BlackjackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
